22d0c7131f02c03b7c6477eedd350ccdbafd08a8
Sanjangeet Singh <s_gpu@yahoo.com>
2019-10-05 20:27:22 +0530

    Added 'Python 2/Time Conversion.py'

---
+++ Python 2/Time Conversion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+time = raw_input()
+if time[-2:] == 'PM' and time[:2] == '12':
+    time = '12' + time[2:-2]
+elif time[-2:] == 'AM' and time[:2] == '12':
+    time = '00' + time[2:-2]
+elif time[-2:] == 'PM':
+    time = str(int(time[:2]) + 12) + time[2:-2]
+else:
+    time = time[:-2]
+print time
